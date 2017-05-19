34e84701e33b5e9965d852c9b96d20f90b89b3d2
ameerthehacker <ameerjhanprof@gmail.com>
2017-05-19 21:40:56 +0530

    Compress assets images and some housekeeping to remove device and carrierwave completely

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,4 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap
-//= require jquery.remotipart
 //= require_tree .
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,14 @@ class ApplicationController < ActionController::Base
   def current_user
       @user ||= User.find(session[:user_id])
   end
+  def authenticate_user!
+      unless user_signed_in?
+          redirect_to home_path
+          false
+      else
+          true
+      end
+  end
 
   helper_method :user_signed_in?
   helper_method :current_user
