d0b40192a866782ff0ceb8ba9dbd755d23ff3f1b
ameerthehacker <ameerjhanprof@gmail.com>
2017-05-23 23:14:54 +0530

    Change the database credentials

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ActiveRecord::Base
     # Validations
-    validates :question, :presence => true, :length => { :minimum => 5, :maximum => 40 }
+    validates :question, :presence => true, :length => { :minimum => 5, :maximum => 200 }
     # Associations
     belongs_to :book
 end
