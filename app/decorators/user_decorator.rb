25921f7a1c685c296750ab1eb3d2dc1ee869bd4d
ameerthehacker <ameerjhanprof@gmail.com>
2017-05-24 19:47:23 +0530

    Fix bug in follow link

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -20,13 +20,15 @@ class UserDecorator < Draper::Decorator
     h.link_to model.full_name, h.user_path(model) 
   end
   def follow_link
-    h.content_tag :div, :class => 'user-follow', :id => "follow-user-#{model.id}" do
-      if Following.where(:follower_id=>h.current_user, :user_id=>user).count==0 
-        unless h.current_user.id == user.id 
-          h.link_to 'Follow', h.user_follow_path(user.id), :remote=>true ,:class=>'btn btn-xs btn-success'
-        end 
-      else 
-        h.link_to 'Unfollow', h.user_unfollow_path(user.id), :remote=>true ,:class=>'btn btn-xs btn-danger'
+    if h.user_signed_in?
+      h.content_tag :div, :class => 'user-follow', :id => "follow-user-#{model.id}" do
+        if Following.where(:follower_id=>h.current_user, :user_id=>user).count==0 
+          unless h.current_user.id == user.id 
+            h.link_to 'Follow', h.user_follow_path(user.id), :remote=>true ,:class=>'btn btn-xs btn-success'
+          end 
+        else 
+          h.link_to 'Unfollow', h.user_unfollow_path(user.id), :remote=>true ,:class=>'btn btn-xs btn-danger'
+        end
       end
     end
   end
