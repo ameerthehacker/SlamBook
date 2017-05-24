76653a7093dcc74249ac34ebbf9590671558064e
ameerthehacker <ameerjhanprof@gmail.com>
2017-05-24 19:23:49 +0530

    Fix bug in news feed pagination

--- app/controllers/news_feeds_controller.rb
+++ app/controllers/news_feeds_controller.rb
@@ -1,8 +1,8 @@
 class NewsFeedsController < ApplicationController
     def index
-        @news_feeds = current_user.news_feeds
+        @news_feeds = current_user.news_feeds.paginate(:page => params[:page], :per_page => 10)
         if @news_feeds.count == 0
-            @news_feeds = NewsFeed.where(:user_id => current_user.fb_friends.ids).order("created_at DESC")
+            @news_feeds = NewsFeed.paginate(:page => params[:page], :per_page => 10).where(:user_id => current_user.fb_friends.ids).order("created_at DESC")
         else
             @news_feeds
         end
