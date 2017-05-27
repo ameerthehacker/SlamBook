ae947d4b8b1444c2e16cf3ae5a8bd0c342e6a65e
ameerthehacker <ameerjhanprof@gmail.com>
2017-05-27 11:33:44 +0530

    Add privacy visible to slam owners and book owners

--- app/decorators/news_feed_decorator.rb
+++ app/decorators/news_feed_decorator.rb
@@ -15,8 +15,18 @@ class NewsFeedDecorator < Draper::Decorator
     end
   end
   def slam_header(heading)
+    if model.slam.book.user == h.current_user
+      book_owner = "You "
+      if model.slam.privacy == 'PUBLIC'
+        book_owner += "publicly"
+      elsif model.slam.privacy == 'PRIVATE'
+        book_owner += "privately"        
+      end
+    else
+      book_owner = h.link_to(model.slam.book.user.full_name, h.user_path(model.slam.book.user))
+    end
     h.content_tag :h4, :class => 'page-header' do
-      model.user.decorate.avatar_image(50) + ' ' + h.link_to(model.user.full_name, h.user_path(model.user)) + ' ' + heading + ' ' + model.slam.book.user.decorate.avatar_image(50) + ' ' + h.link_to(model.slam.book.user.full_name, h.user_path(model.slam.book.user))
+      model.user.decorate.avatar_image(50) + ' ' + h.link_to(model.user.full_name, h.user_path(model.user)) + ' ' + heading + ' ' + model.slam.book.user.decorate.avatar_image(50) + ' ' + book_owner
     end
   end
 end
